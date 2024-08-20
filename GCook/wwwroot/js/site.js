381a523a573d154f0692a35a9c72480df993c07d
GalloJunior <gallojunior@gmail.com>
2024-08-20 10:50:49 -0300

    Criando a página de Receita

--- GCook/wwwroot/js/site.js
+++ GCook/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function slash(id) {
+    let name = "check-" + id;
+    let elem = document.getElementById(name);
+    let label = $('label[for="'+ name +'"]')
+    if (elem.checked) {
+        label.addClass('traced')
+    } else {
+        label.removeClass('traced')
+    }
+}
